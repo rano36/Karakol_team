c1e8f635d66aaf20d6ba6ca5d0848a6b7ef05d3d
Saida <your_github_username@users.noreply.github.com>
2025-03-31 16:30:29 +0600

    Feature / scrum23 task commit

---
+++ src/main/java/Saida/right2.java
@@ -0,0 +1,10 @@
+package Saida;
+
+public class right2 {
+    public String right2(String str) {
+        if (str.length() > 2) {
+            return str.substring(str.length()-2)
+                    .concat(str.substring(0,str.length()-2));
+        }return str;
+    }
+}

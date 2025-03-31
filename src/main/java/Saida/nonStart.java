99e40a00d3b145010055e07b3aabc95cc71cf057
Saida <your_github_username@users.noreply.github.com>
2025-03-31 16:33:19 +0600

    Feature / scrum23 task commit

--- src/main/java/Saida/nonStart.java
+++ src/main/java/Saida/nonStart.java
@@ -2,8 +2,9 @@ package Saida;
 
 public class nonStart {
     public String nonStart(String a, String b) {
-        if (a.length() >=1 && b.length() >=1 ){
-            return a.substring(1,a.length()).concat(b.substring(1,b.length()));
-        } return a.concat(b);
+        if (a.length() >= 1 && b.length() >= 1) {
+            return a.substring(1, a.length()).concat(b.substring(1, b.length()));
+        }
+        return a.concat(b);
     }
 }
--- src/main/java/Saida/right2.java
+++ src/main/java/Saida/right2.java
@@ -3,8 +3,9 @@ package Saida;
 public class right2 {
     public String right2(String str) {
         if (str.length() > 2) {
-            return str.substring(str.length()-2)
-                    .concat(str.substring(0,str.length()-2));
-        }return str;
+            return str.substring(str.length() - 2)
+                    .concat(str.substring(0, str.length() - 2));
+        }
+        return str;
     }
 }

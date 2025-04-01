db13d182d193d4f5e8f39774e12ef82c5f363029
Saida <your_github_username@users.noreply.github.com>
2025-04-01 14:54:59 +0600

    Добавлено решение задачи с условием менее 2 символов

--- src/main/java/Saida/String1/makeOutWord.java
+++ src/main/java/Saida/String1/makeOutWord.java
@@ -3,6 +3,5 @@ package Saida.String1;
 public class makeOutWord {
     public String makeOutWord(String out, String word) {
         return out.substring(0, 2) + word + out.substring(2, 4);
-
     }
 }

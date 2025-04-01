375e98601b39cad4ef44dad50a0cd971cabb2cca
Rano <ranotoktorbaeva@gmail.com>
2025-04-01 14:55:49 +0600

    Fix/Rano/добавила условие и удалила пробелы

--- src/main/java/rano_scrum_31/WithoutEnd2.java
+++ src/main/java/rano_scrum_31/WithoutEnd2.java
@@ -1,8 +1,8 @@
-package scrum_31;
+package rano_scrum_31;
 
 public class WithoutEnd2 {
     public String withouEnd2(String str) {
-        if (str.length() <= 2){
+        if (str.length() <= 2) {
             return "";
         }
         return str.substring(1, str.length() - 1);
--- src/main/java/scrum_31/Right2.java
+++
@@ -1,7 +0,0 @@
-package scrum_31;
-
-public class Right2 {
-    public String right2(String str) {
-        return str.substring(str.length()-2)+ str.substring(0, str.length()-2);
-    }
-}

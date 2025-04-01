27944ec55ad399dd8b07b85a060a913c6f00257b
Rano <ranotoktorbaeva@gmail.com>
2025-04-01 14:21:29 +0600

    Fix/Rano-SCRUM-55/форматирование кода в IDE

---
+++ src/main/java/rano_string_1/EndsLy.java
@@ -0,0 +1,11 @@
+package rano_string_1;
+
+public class EndsLy {
+    public boolean endsLy(String str) {
+        if (str.endsWith("ly")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}

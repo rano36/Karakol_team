dfb48c1cff4844b116d8c33fbc59488a7a50db5c
Rano <ranotoktorbaeva@gmail.com>
2025-04-01 10:04:01 +0600

    Rano/String1/SCRUM-31

---
+++ src/main/java/scrum_31/WithoutEnd2.java
@@ -0,0 +1,10 @@
+package scrum_31;
+
+public class WithoutEnd2 {
+    public String withouEnd2(String str) {
+        if (str.length() <= 2){
+            return "";
+        }
+        return str.substring(1, str.length() - 1);
+    }
+}

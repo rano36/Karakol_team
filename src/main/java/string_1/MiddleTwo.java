7b1a9946dc11a65d351770a2271d3eb18b970e62
Rano <ranotoktorbaeva@gmail.com>
2025-04-02 18:14:15 +0600

    Удаляю комиты чтобы перезаписать в свою папку "Merge pull request #11 from rano36/SCRUM_55"
    This reverts commit 06d3d516668ec7aecefda7cac9eeeabd02386c47, reversing
    changes made to d177e58faa2dd095824ad69c8e4cd49aa442f3d0.

--- src/main/java/string_1/MiddleTwo.java
+++
@@ -1,9 +0,0 @@
-package String_1;
-
-public class MiddleTwo {
-    public String middleTwo(String str){
-        int n = str.length();
-        int mid = n / 2;
-        return str.substring(mid - 1, mid + 1);
-    }
-}

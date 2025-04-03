7a9c4d7a31e0372a668204237d1e4294ac9cb262
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-04-03 10:34:21 +0600

    Fix/scrum_49/changes in directory name commit

--- src/main/java/Akylai/Map1/Topping1.java
+++ src/main/java/Akylai/Map1/Topping1.java
@@ -1,9 +1,12 @@
-package Akylai_codingbat.Map1;
+package Akylai.Map1;
 
 import java.util.Map;
 
 public class Topping1 {
     public Map<String, String> topping1(Map<String, String> map) {
+        if (map == null) {
+            return null;
+        }
         if (map.containsKey("ice cream")) {
             map.replace("ice cream", "cherry");
         }
--- src/main/java/Akylai/Map1/Topping2.java
+++ src/main/java/Akylai/Map1/Topping2.java
@@ -1,8 +1,8 @@
-package Akylai_codingbat.Map1;
+package Akylai.Map1;
 
 import java.util.Map;
 
-public class Topping3 {
+public class Topping2 {
     public Map<String, String> topping2(Map<String, String> map) {
         if (map.containsKey("ice cream")) {
             map.put("yogurt", map.get("ice cream"));

58ecca4b2316d2a68950fc4c551f5cebc0c8db66
Rano <ranotoktorbaeva@gmail.com>
2025-04-08 19:35:52 +0600

    feature/add 3 tasks

---
+++ src/main/java/rano/warmap1/Front22.java
@@ -0,0 +1,13 @@
+package rano.warmap1;
+
+public class Front22 {
+    public String front22(String str) {
+        String front;
+        if (str.length() >= 2) {
+            front = str.substring(0, 2);
+        } else {
+            front = str;
+        }
+        return front + str + front;
+    }
+}
---
+++ src/main/java/rano/warmap1/StartHi.java
@@ -0,0 +1,7 @@
+package rano.warmap1;
+
+public class StartHi {
+    public boolean startHi(String str) {
+    return str.startsWith("hi");
+    }
+}

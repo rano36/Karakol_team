dde7a9d85fb98b084c26c9b8eb103caefa20d49d
Rano <ranotoktorbaeva@gmail.com>
2025-04-08 10:10:09 +0600

    Fix/added IllegalArgumentException in MiddleThree

--- src/main/java/rano/String_1/MiddleTwo.java
+++
@@ -1,8 +0,0 @@
-package rano.String_1;
-
-public class MiddleTwo {
-    public String middleThree(String str) {
-        int mis = str.length() / 2;
-        return str.substring(mis - 1, mis + 2);
-    }
-}

987fb94ade516cf31042186bc8863345065eb652
Saida <your_github_username@users.noreply.github.com>
2025-04-09 10:33:39 +0600

    Fixed mistakes

--- src/main/java/Saida/String1/twoChar.java
+++ src/main/java/Saida/String1/twoChar.java
@@ -1,4 +1,11 @@
 package Saida.String1;
 
 public class twoChar {
+    public String twoChar(String str, int index) {
+        if (index >= 0 && index <= str.length() - 2) {
+            return str.substring(index, index + 2);
+        } else {
+            return str.substring(0, 2);
+        }
+    }
 }

fe0c37bdb7574abe9708c7df75d099c2a75f72a8
Aiperi06 <aibige24wka@gmail.com>
2025-04-15 00:03:00 +0600

    SCRUM-156

---
+++ src/main/java/Aiperi/functional_1/AddStar.java
@@ -0,0 +1,10 @@
+package Aiperi.functional_1;
+
+import java.util.List;
+
+public class AddStar {
+    public List<String> addStar(List<String> strings) {
+        strings.replaceAll(n -> n + "*");
+        return strings;
+    }
+}
---
+++ src/main/java/Aiperi/functional_1/Copies3.java
@@ -0,0 +1,10 @@
+package Aiperi.functional_1;
+
+import java.util.List;
+
+public class Copies3 {
+    public List<String> copies3(List<String> strings) {
+        strings.replaceAll(n ->  n + n + n);
+        return strings;
+    }
+}

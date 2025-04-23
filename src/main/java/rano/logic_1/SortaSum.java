397cf299e2d96773794cc0db659ce30ecc6f8e22
Rano <ranotoktorbaeva@gmail.com>
2025-04-23 10:25:07 +0600

    Feature/add 3 new tasks

---
+++ src/main/java/rano/logic_1/SortaSum.java
@@ -0,0 +1,12 @@
+package rano.logic_1;
+
+public class SortaSum {
+    public int sortaSum(int a, int b) {
+        int sum = a + b;
+        if (sum >= 10 && sum <= 19) {
+            return 20;
+        } else {
+            return sum;
+        }
+    }
+}

c0805502c90ae09a3ac0acc0fb9b87574a843704
Begayim <begayimmusakeeva@gmail.com>
2025-04-23 18:42:40 +0700

    Задачи 13,14,15 из Рекурсии 1

---
+++ src/main/java/begayim/recursion1/rec_13_14_15.java
@@ -0,0 +1,36 @@
+package begayim.recursion1;
+
+public class rec_13_14_15 {
+
+    public String changePi (String str){
+        if (str.length() < 2){
+            return str;
+        }
+        if (str.startsWith("pi")){
+            return "3.14" + changePi(str.substring(2));
+        } else {
+            return str.charAt(0) + changePi(str.substring(1));
+        }
+    }
+
+    public String noX(String str){
+        if (str.isEmpty()){
+            return str;
+        }
+        if (str.charAt(0) == 'x'){
+            return noX(str.substring(1));
+        }else {
+            return str.charAt(0) + noX(str.substring(1));
+        }
+    }
+
+    public boolean array6(int[] nums, int index){
+        if (index >= nums.length){
+            return false;
+        }
+        if (nums[index] == 6){
+            return true;
+        }
+        return array6(nums, index + 1);
+    }
+}

f408b796c574e061fd830d4fed4143cfb7761c58
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-05-04 16:53:08 +0600

    Feature/CPG-29/logic1/tasks commit

---
+++ src/main/java/akylai/logic1/Less20.java
@@ -0,0 +1,7 @@
+package akylai.logic1;
+
+public class Less20 {
+    public boolean less20(int n) {
+        return n % 20 == 18 || n % 20 == 19;
+    }
+}
---
+++ src/main/java/akylai/logic1/Old35.java
@@ -0,0 +1,7 @@
+package akylai.logic1;
+
+public class Old35 {
+    public boolean old35(int n) {
+        return (n % 3 == 0 || n % 5 == 0) && !(n % 3 == 0 && n % 5 == 0);
+    }
+}

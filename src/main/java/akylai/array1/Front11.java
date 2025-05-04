c1a0ac28b209b638f1e955d8a78e55a76c8103bd
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-05-04 16:19:06 +0600

    Fix/added changes to task Front11

--- src/main/java/akylai/array1/Front11.java
+++ src/main/java/akylai/array1/Front11.java
@@ -5,7 +5,6 @@ public class Front11 {
         if (a.length == 0 && b.length == 0) return new int[]{};
         if (a.length != 0 && b.length == 0) return new int[]{a[0]};
         if (a.length == 0 && b.length != 0) return new int[]{b[0]};
-
         return new int[]{a[0], b[0]};
     }
 }

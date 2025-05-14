a1d60f697107ca21e90b639ddba6b3d5531a7db2
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-05-14 09:02:44 +0600

    Feature/CPG-204/Logic-1 tasks commit

---
+++ src/main/java/akylai/logic1/InOrderEqual.java
@@ -0,0 +1,12 @@
+package akylai.logic1;
+
+public class InOrderEqual {
+    public boolean inOrderEqual(int a, int b, int c, boolean equalOk) {
+        if (!equalOk && a < b && b < c) {
+            return true;
+        } else if (equalOk && a <= b && b <= c) {
+            return true;
+        }
+        return false;
+    }
+}

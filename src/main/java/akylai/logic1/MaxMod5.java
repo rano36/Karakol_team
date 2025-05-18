d82dd688ee7bf040b1145a9b078c982420d955ff
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-05-18 13:32:01 +0600

    Feature/CPG-205/Logic-1 tasks commit

---
+++ src/main/java/akylai/logic1/MaxMod5.java
@@ -0,0 +1,13 @@
+package akylai.logic1;
+
+public class MaxMod5 {
+    public int maxMod5(int a, int b) {
+        if (a == b) {
+            return 0;
+        }
+        if (a % 5 == b % 5) {
+            return Math.min(a, b);
+        }
+        return Math.max(a, b);
+    }
+}
---
+++ src/main/java/akylai/logic1/WithoutDoubles.java
@@ -0,0 +1,10 @@
+package akylai.logic1;
+
+public class WithoutDoubles {
+    public int withoutDoubles(int die1, int die2, boolean noDoubles) {
+        if (noDoubles && die1 == die2) {
+            die1 = (die1 == 6) ? 1 : die1 + 1;
+        }
+        return die1 + die2;
+    }
+}

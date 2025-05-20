bc23dc4cfbf1e818c1b629d93239061e3eb06b8c
Akylai <192575286+Akylai89@users.noreply.github.com>
2025-05-20 09:04:38 +0600

    Feature/CPG-207/Logic-1 last tasks commit

---
+++ src/main/java/akylai/logic1/ShareDigit.java
@@ -0,0 +1,15 @@
+package akylai.logic1;
+
+public class ShareDigit {
+    public boolean shareDigit(int a, int b) {
+        int aLeft = a / 10;
+        int aRight = a % 10;
+        int bLeft = b / 10;
+        int bRight = b % 10;
+        if (aLeft == bLeft || aLeft == bRight || aRight == bLeft || aRight == bRight) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
